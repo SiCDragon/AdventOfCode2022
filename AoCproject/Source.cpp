14fe6265df4fde2e4c61e32d3565851ce8a6784a
SiCDragon <dunga89@gmail.com>
2022-12-01 20:30:02 -0500

    Day 1 again

--- AoCproject/Source.cpp
+++ AoCproject/Source.cpp
@@ -1,14 +1,20 @@
 #include <fstream>
 #include <iostream>
 #include <string>
+#include <map>
 
 int main()
 {
     // ifstream is used for reading files
     std::ifstream potato{ "elves.txt" };
     std::string storedInput = "";
-    int elf{ 0 };
-    int thiccElf{ 0 };
+    int elfCalories{ 0 };
+    int bigCals{ 0 };
+
+    std::map<int, int> elves{ {1,1} };
+    std::map<std::string, int> thiccElves{{"thicc", 0}, {"thiccer", 0}, {"thiccest", 0}};
+
+    int elfNumber{ 1 };
 
     // If we couldn't open the input file stream for reading
     if (!potato)
@@ -27,23 +33,41 @@ int main()
         
         if (!strInput.empty()){
             
-            elf += std::stoi(strInput);
+            elfCalories += std::stoi(strInput);
             
-            if (elf > thiccElf)
-            {
-                thiccElf = elf;
-                std::cout << "Thicc: " << thiccElf << '\n';
-            }
         }
         else
         {
-            elf = 0;
+            if (elfCalories > thiccElves["thiccest"])
+            {
+                thiccElves["thiccer"] = thiccElves["thiccest"];
+                thiccElves["thiccest"] = elfCalories;
+
+            }
+            else if (elfCalories < thiccElves["thiccest"] && elfCalories > thiccElves["thiccer"])
+            {
+                thiccElves["thicc"] = thiccElves["thiccer"];
+                thiccElves["thiccer"] = elfCalories;
+            }
+            else if (elfCalories < thiccElves["thiccer"] && elfCalories > thiccElves["thicc"])
+            {
+                thiccElves["thicc"] = elfCalories;
+            }
+
+            //elves.insert(elfNumber, elfCalories);
+            elfCalories = 0;
+            elfNumber++;
         }
 
         //storedInput.append(strInput);
         //std::cout << "Elf: " << elf << '\n';
         //std::cout << "Thicc: " << thiccElf << '\n';
-
+        
     }
+    bigCals = thiccElves["thicc"] + thiccElves["thiccer"] + thiccElves["thiccest"];
+    std::cout << "Thicc: " << thiccElves["thicc"] << '\n';
+    std::cout << "Thiccer: " << thiccElves["thiccer"] << '\n';
+    std::cout << "Thiccest: " << thiccElves["thiccest"] << '\n';
+    std::cout << "Big Calories: " << bigCals << '\n';
     return 0;
 }
